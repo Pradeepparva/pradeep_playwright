91a3df9133f620bec68ebd1343ff135792d46425
Pradeepparva <pradeepparva@gmail.com>
2024-11-21 16:30:18 +0530

    second time

--- tests/DemoApp.spec.js
+++ tests/DemoApp.spec.js
@@ -4,5 +4,5 @@ test('keyboard action',async({page})=>{
     await page.goto("https://demoapps.qspiders.com/ui/keyboard?sublist=0")
     await page.locator("//input[@name='handleInput']").press('Enter')
     await page.pause(3000);
-
+    
 })
---
+++ tests/Flipkart.spec.js
@@ -0,0 +1,39 @@
+import { test,expect } from "@playwright/test";
+// to fetch the price or product names displayed in the page
+// www.flipkart.com
+
+test('Fetch product list and price',async({page})=>{
+
+    //Launching the Application and validating whether right page is opened.
+    await page.goto("https://www.flipkart.com/")
+    await expect(page).toHaveTitle("Online Shopping Site for Mobiles, Electronics, Furniture, Grocery, Lifestyle, Books & More. Best Offers!")
+
+    await page.fill('[name="q"]',"iphone 14 5g")
+    await page.keyboard.press("Enter")
+
+    await page.waitForSelector("//div['container']/div[@class='KzDlHZ']")
+    let products=await page.$$("//div['container']/div[@class='KzDlHZ']")
+    let men=[]
+    for(let product of products){
+        let load=await product.textContent()
+        men.push(load)
+    }
+    await page.waitForSelector("//div['container']/div[@class='Nx9bqj _4b5DiR']")
+    let prices=await page.$$("//div['container']/div[@class='Nx9bqj _4b5DiR']")
+    let women=[]
+    for(let price of prices){
+        let pox=await price.textContent()
+        women.push(pox)
+    }
+    console.log(men)
+    console.log(women)
+})
+
+//to show the dynamic brand which we choose (dynamic xpath)
+// www.amazon.com
+test('dynamic X-path',async({page})=>{
+    await page.goto("www.amazon.com")
+    // await expect(page).toHaveTitle("")
+})
+
+
