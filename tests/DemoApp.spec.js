eb2d7a5f6a43386e6e1b82eb122c7d91dd800efd
Pradeepparva <pradeepparva@gmail.com>
2024-11-21 12:45:07 +0530

    first commit

---
+++ tests/DemoApp.spec.js
@@ -0,0 +1,8 @@
+const {test,expect}=require("@playwright/test")
+
+test('keyboard action',async({page})=>{
+    await page.goto("https://demoapps.qspiders.com/ui/keyboard?sublist=0")
+    await page.locator("//input[@name='handleInput']").press('Enter')
+    await page.pause(3000);
+
+})
